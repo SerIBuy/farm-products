36fadcf584e57bc473bbf8d5de464a4f3e1594a4
Sergey Buy <154270878+SerIBuy@users.noreply.github.com>
2024-09-04 13:24:59 +0300

    что-то

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,7 +1,13 @@
-import './Button.css';
+import style from "./Button.module.css";
 
 export default function Button({ children, minWidth }) {
-    return (
-        <button type="button" style={{ minWidth: minWidth }} className="button">{children}</button>
-    )
+  return (
+    <button
+      type="button"
+      style={{ minWidth: minWidth }}
+      className={style.button}
+    >
+      {children}
+    </button>
+  );
 }
