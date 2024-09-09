76b98c4dc7d57ccf40430fb089b59aaf726f8f62
Sergey Buy <154270878+SerIBuy@users.noreply.github.com>
2024-09-09 20:54:14 +0300

    в процессе стилизации

--- src/components/app/styles.js
+++ src/components/app/styles.js
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from "styled-components";
+import { defaultTheme } from "../../themes/default";
 
 export const GlobalStyle = createGlobalStyle`
 
 body {
-  font-family: var(--general-font);
+  font-family: ${(props) => props.theme.fontFamily};
   font-weight: 400;
   font-size: 18px;
   line-height: 27px;
